90c278458b24c51bc4dc2d8534efa86d8ccf5eab
Ayibatari Ibaba <tariibabadev@gmail.com>
2022-05-26 11:41:45 +0100

    Fix issue where future reminders don't move to "Stopped" group when stopped

--- src/components/reminder-list-by-date.tsx
+++ src/components/reminder-list-by-date.tsx
@@ -16,10 +16,12 @@ type ReminderListByDateProps = {
 };
 
 const ReminderListByDate = (props: ReminderListByDateProps) => {
-  const reminders = props.reminders.sort(
-    (reminder1, reminder2) =>
-      reminder1.remindTime.getTime() - reminder2.remindTime.getTime()
-  );
+  const reminders = props.reminders
+    .filter((reminder) => !reminder.stopped)
+    .sort(
+      (reminder1, reminder2) =>
+        reminder1.remindTime.getTime() - reminder2.remindTime.getTime()
+    );
   const overdue = reminders.filter((reminder) => isDue(reminder));
   const today = reminders.filter(
     (reminder) => !isPast(reminder) && isToday(reminder)
